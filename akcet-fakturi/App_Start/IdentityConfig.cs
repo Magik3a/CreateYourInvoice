2682cd8b13fa235e7b164be15e92843f4e041bc0
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-06-11 12:13:02 +0300

    .

--- akcet-fakturi/App_Start/IdentityConfig.cs
+++ akcet-fakturi/App_Start/IdentityConfig.cs
@@ -54,16 +54,16 @@ namespace akcet_fakturi
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            manager.MaxFailedAccessAttemptsBeforeLockout = 3;
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug it in here.
