15fa5dbe7ddd92bbb733ab164c596f8a75fc8e38
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-06-19 00:24:12 +0300

    .

---
+++ akcet-fakturi/Models/InvoicesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using akcetDB;
+
+namespace akcet_fakturi.Models
+{
+    public class InvoicesViewModel
+    {
+        public Company Companies { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
