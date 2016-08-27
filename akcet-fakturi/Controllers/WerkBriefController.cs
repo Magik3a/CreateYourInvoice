a875a2b10f19d3bcf436025b07f88b2eb0030e00
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-08-27 16:32:53 +0300

    Added pages for werkbriefs and controllers for admin and user

---
+++ akcet-fakturi/Controllers/WerkBriefController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace akcet_fakturi.Controllers
+{
+    public class WerkBriefController : Controller
+    {
+        // GET: WerkBrief
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
